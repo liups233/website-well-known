4bca9170f8927454548f0aaf67242d3673077b81
Liups233 <admin@liups.net>
2026-06-11 18:50:51 +0800

    Update sh file

--- openpgpkey.sh
+++ openpgpkey.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 gpg --list-options show-only-fpr-mbox -k "admin@liups.net" | gpg-wks-client -v --directory .well-known/openpgpkey --install-key
+echo ""
+echo "To generate OPENPGPKEY DNS record, install hash-slinger and run openpgpkey --create admin@liups.net"
+
